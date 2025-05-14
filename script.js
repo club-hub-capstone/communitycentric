1d514a34c701525b4b26c1c050ef942621a3b824
vvill <contact@vvill.dev>
2025-05-13 19:11:06 -0700

    Fix non-persistance of theme

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 console.log("Hello, World!");
 
 const themeCheckbox = document.querySelector("input[name='theme']");
-let theme = window.localStorage.getItem("theme") ?? window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light";
+let theme = window.localStorage.getItem("theme");
+if (!theme)
+  theme = window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light";
+
+// Set theme on load
+document.documentElement.setAttribute("data-theme", theme);
 themeCheckbox.checked = theme === "dark";
+
+// Add event listener to theme checkbox
 themeCheckbox.addEventListener("change", (e) => {
   theme = e.target.checked ? "dark" : "light";
   document.documentElement.setAttribute("data-theme", theme);
